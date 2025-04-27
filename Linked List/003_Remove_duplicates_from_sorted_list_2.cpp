69197dcf6f157ca711cc1f67ea40abc7bf5eb08f
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-04-27 12:28:06 +0530

    Linked List 003: Remove duplicates from sorted linked list 2

---
+++ Linked List/003_Remove_duplicates_from_sorted_list_2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <gtest/gtest.h>
+
+struct Node {
+    int data;
+    std::shared_ptr<Node> next;
+
+    Node(int x): data(x), next(nullptr) {}
+};
+
+auto remove_duplicates_from_sorted_list(std::shared_ptr<Node> head) {
+    // Return if head is nullptr
+    if(!head) {
+        return std::shared_ptr<Node>();
+    }
+
+    std::shared_ptr<Node> dummy = std::make_shared<Node>(0);
+    dummy->next = head;
+    std::shared_ptr<Node> current = dummy;
+
+    // Delete nodes with duplicate values completely
+    while(current->next && current->next->next) {
+        if(current->next->data == current->next->next->data) {
+            int duplicate = current->next->data;
+
+            while(current->next && current->next->data == duplicate) {
+                current->next = current->next->next;
+            }
+        }
+        else {
+            current = current->next;
+        }
+    }
+
+    return dummy->next;
+}
+
+//Function to convert a vector to a linked list
+auto vector_to_list(const std::vector<int>& nums) {
+    std::shared_ptr<Node> current = std::make_shared<Node>(0);
+    std::shared_ptr<Node> dummy = current;
+
+    for(int i: nums) {
+        dummy->next = std::make_shared<Node>(i);
+        dummy = dummy->next;
+    }
+
+    return current->next;
+}
+
+//Function to convert a linked list to a vector
+auto list_to_vector(std::shared_ptr<Node> node) {
+    std::vector<int> result;
+
+    while(node) {
+        result.push_back(node->data);
+        node = node->next;
+    }
+
+    return result;
+}
+
+void test(const std::vector<int>& head_array, const std::vector<int>& expected_array) {    
+    std::shared_ptr<Node> head = vector_to_list(head_array);
+    std::shared_ptr<Node> result = remove_duplicates_from_sorted_list(head);
+    std::vector<int> result_array = list_to_vector(result);
+
+    EXPECT_EQ(result_array, expected_array);
+}
+
+TEST(Remove_duplicates_from_sorted_list, handles_various_cases) {
+    test({1, 2, 3, 3, 4, 4, 5}, {1, 2, 5});
+    test({1, 1, 1, 2, 3}, {2, 3});
+}
